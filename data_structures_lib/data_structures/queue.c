b4377fae2becd9da001bc6b7ced15f8907bfb5b6
Nino Singh <zgirl0007@gmail.com>
2019-11-05 16:22:59 -0500

    Fixed small bug in the queue api. Remove function doesn't return anything after its removed and deleted the node from the queue.

--- data_structures_lib/data_structures/queue.c
+++ data_structures_lib/data_structures/queue.c
@@ -16,10 +16,9 @@ void queue_add(Queue *q, void *data)
 }
 
 //Remove element from front and return head of queue
-void * queue_remove(Queue *q)
+void queue_remove(Queue *q)
 {
   list_remove(q->list, 0);
-  return queue_peek(q);
 }
 
 //Get reference to head of queue
