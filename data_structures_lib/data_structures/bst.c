227bd67c88c5d77c91d0b757457217402e6b88b7
Nino Singh <zgirl0007@gmail.com>
2019-11-06 11:59:37 -0500

    Created binary search tree interface

--- data_structures_lib/data_structures/bst.c
+++ data_structures_lib/data_structures/bst.c
@@ -13,93 +13,40 @@ BstNode *create_node(void *key, void*data)
 }
 
 //Insert a new node in the binary search tree
-void bstree_insert(Bstree *bst, void *key, void *data)
-{
-
-}
+void bstree_insert(Bstree *bst, void *key, void *data) {}
 
 //Remove node with given key
-void bstree_remove(Bstree *bst, void *key)
-{
+void bstree_remove(Bstree *bst, void *key) {}
 
-}
+//Find node with smallest key
+BstNode * bstree_getMin(Bstree *bst) {}
 
-//Find node with given key an return data
-void * bstree_retrieve(Bstree *bst, void *key)
-{
-  return NULL;
-}
+//Find node with largest key
+BstNode * bstree_getMax(Bstree *bst) {}
 
 //Check if node exists with given key
-bool bstree_contains(Bstree *bst, void *key)
-{
-  return false;
-}
+BstNode * bstree_contains(Bstree *bst, void *key) {}
+
+//Check if node exists with given key
+bool bstree_isFull(Bstree *bst) {return false;}
+
+//Check if node exists with given key
+bool bstree_isBalanced(Bstree *bst) {return false;}
+
+//Get height of the tree
+bool bstree_getHeight(Bstree *bst) {return false;}
 
 //Return number of nodes in tree
-int bstree_size(Bstree *bst)
-{
-  return -1;
-}
-
-//Return true if tree is empty
-bool bstree_isEmpty(Bstree *bsst)
-{
-  return false;
-}
+int bstree_size(Bstree *bst) {return 1;}
 
 //
-void bstree_preorder(Bstree *bst)
-{
-
-}
+void bstree_preorder(Bstree *bst) {}
 
 //
-void bstree_postorder(Bstree *bst)
-{
-
-}
+void bstree_postorder(Bstree *bst) {}
 
 //
-void bstree_inorder(Bstree *bst)
-{
+void bstree_inorder(Bstree *bst) {}
 
-}
-
-//Print nodes in level order traveseral
-void bstree_print(Bstree *bst, void (*printNode)(void *))
-{
-  if(bst->root == NULL) return;
-
-  Queue q;
-  List list;
-
-  q.list = &list;
-  BstNode *root = bst->root;
-  queue_add(&q, root);
-
-  while(queue_size(&q) > 0)
-  {
-    int nodeCount = queue_size(&q);
-
-    while(nodeCount > 0)
-    {
-      BstNode *node = (BstNode *) queue_peek(&q);
-      (*printNode)(node->data);
-      queue_remove(&q);
-
-      if(node->left != NULL)
-      {
-        queue_add(&q, node->left);
-      }
-      if(node->right != NULL)
-      {
-        queue_add(&q, node->right);
-      }
-      nodeCount--;
-    }
-    printf("\n");
-  }
-}
-
-//Find min value of node in the tree
+//Print nodes of binary search tree
+void bstree_print(Bstree *bst, void (*printNode)(void *)) {}
