1db9b0eb83109330f73c874d0f9e407f023676bc
Nino Singh <zgirl0007@gmail.com>
2020-01-31 15:43:47 -0500

    Added implementation for mem_alloc/free and helper functions to find free block in list, request new memory from static pool

--- MemoryManagement/MemAllocator.c
+++ MemoryManagement/MemAllocator.c
@@ -8,44 +8,22 @@ static mem_block_header *base;
 //Index position in the statically allocated array of memory
 static int free_pool_pos;
 
+//Initialize our static memory pool
+static char pool[MEM_POOL_SIZE] = {0};
+
 //Helper functions
 static void* request_mem(mem_block_header *last, size_t size);
-static void* split(mem_block_header* b, size_t allocSize);
+static mem_block_header* find_free_block(mem_block_header **last, size_t size);
+static mem_block_header* split(mem_block_header* b, size_t allocSize);
 static void merge(void);
 
 /**
 * Initialize head of free list
 */
 void mem_init(void) {
-  return;
+  base = NULL;
+  free_pool_pos = 0;
 }
-
-/**
-* Used to request more memory from the heap which is managed by the OS
-* In this case, we have a large statically allocated array as the "heap"
-* If there are no free blocks that have been malloced and freed for reuse,
-* request more space from this allocation pool
-*/
-static void* request_mem(mem_block_header *last, size_t size)
-{
-  return NULL;
-}
-
-/**
-* If we found a free block to use and its bigger than the memory
-* we requested, split the free block and return a block of exact size
-*/
-static void* split(mem_block_header* b, size_t allocSize)
-{
-  return NULL;
-}
-
-/**
-* This would get called if there is no free block that is greater than or
-* equal to the requested memory. Merge one or more blocks together to form
-* a free block to allocate.
-*/
-static void merge(void);
 
 /**
 * Allocate memory of requested size. Search the free blocks list to see if
@@ -54,7 +32,40 @@ static void merge(void);
 */
 void *mem_alloc(size_t size)
 {
-  return NULL;
+  if(size <= 0) {
+    return NULL;
+  }
+
+  mem_block_header *blockp = NULL;
+
+  //First call, no free blocks in list yet, request memory from our pool
+  if(base == NULL) {
+    blockp = (mem_block_header*)request_mem(NULL, size);
+
+    if(blockp == NULL) {
+      return NULL;
+    } else {
+      base = blockp;
+    }
+  } else {
+    mem_block_header* last = base;
+    blockp = find_free_block(&last, size);
+
+    if(blockp == NULL) {
+      //If no free block exists, request memory from our pool
+      blockp = request_mem(last, size);
+
+      if(blockp == NULL) {
+        return NULL;
+      } else {
+        //Todo: Consider splitting block so we give a block of exact size
+        //instead of wasting the whole block which could be larger
+        blockp->free = 0;
+      }
+    }
+  }
+  //return pointer to allocated block space, skip over the block header
+  return (blockp + 1);
 }
 
 
@@ -65,6 +76,83 @@ void *mem_alloc(size_t size)
 * free list
 */
 void mem_free(void* ptr)
+{
+  if(ptr == NULL) {
+    return;
+  }
+  mem_block_header* blockp = (mem_block_header*)(ptr-1);
+  assert(blockp->free == 0); //should be equal to 0!
+  blockp->free = 1;
+}
+
+static void* pool_alloc(size_t size) {
+
+  if(size == 0) {
+    return (void *)&pool[free_pool_pos];
+  }
+
+  void *freep = (void*)&pool[free_pool_pos];
+  free_pool_pos+=size;
+
+  if(free_pool_pos >= MEM_POOL_SIZE) {
+      return NULL;
+  }
+  return freep;
+}
+
+/**
+* Used to request more memory from the heap which is managed by the OS
+* In this case, we have a large statically allocated array as the "heap"
+* If there are no free blocks that have been malloced and freed for reuse,
+* request more space from this allocation pool
+*/
+static void* request_mem(mem_block_header *last, size_t size)
+{
+  mem_block_header* blockp = pool_alloc(0);
+  void* requestedp = pool_alloc(size+sizeof(mem_block_header));
+
+  assert((void*)blockp == requestedp);
+
+  if(requestedp == (void*) -1) {
+    return NULL; //pool_alloc failed
+  }
+
+ //NULL on First request
+  if(last != NULL) {
+    last->next = blockp;
+  }
+  blockp->size = size;
+  blockp->next = NULL;
+  blockp->free = 0;
+
+  return blockp;
+}
+
+static mem_block_header* find_free_block(mem_block_header **last, size_t size) {
+  mem_block_header *blockp = base;
+
+  while(blockp != NULL && !(blockp->free && blockp->size>=size)) {
+    *last = blockp;
+    blockp = blockp->next;
+  }
+  return blockp;
+}
+
+/**
+* If we found a free block to use and its bigger than the memory
+* we requested, split the free block and return a block of exact size
+*/
+static mem_block_header* split(mem_block_header* b, size_t allocSize)
+{
+  return NULL;
+}
+
+/**
+* This would get called if there is no free block that is greater than or
+* equal to the requested memory. Merge one or more blocks together to form
+* a free block to allocate.
+*/
+static void merge(void)
 {
   return;
 }
--- MemoryManagement/main.c
+++ MemoryManagement/main.c
@@ -1,5 +1,5 @@
 #include "MemAllocator.h"
 
 int main(void) {
-  return 0; 
+  return 0;
 }
